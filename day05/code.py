ee7790aab3d2fc4d63d7c0085e609ba20e19354d
Sjoerd Nijhof <sjoerd.vdbos@hotmail.com>
2021-12-05 21:49:18 +0100

    d5p2

--- day05/code.py
+++ day05/code.py
@@ -1,11 +1,10 @@
-with open('test1.txt') as f:
-    N = 10
-#with open('input.txt') as f:
-    # N = 1000
+# with open('test1.txt') as f:
+    # N = 10
+with open('input.txt') as f:
+    N = 1000
     lines = f.readlines()
     values = [i.replace('\n','') for i in lines]
 
-N = 10
 map = [[0]*N for _ in range(N)]
 
 def printMap():
@@ -20,19 +19,28 @@ for line in values:
     ey = int(line[line[4:].find(',')+4+1:])
     
     # sample points
-    [sx,ex] = [sx,ex] if sx < ex else [ex,sx]
-    [sy,ey] = [sy,ey] if sy < ey else [ey,sy]    
+    [ssx,eex] = [sx,ex] if sx < ex else [ex,sx]
+    [ssy,eey] = [sy,ey] if sy < ey else [ey,sy]    
 #    print(line)
     
     # fill map, non diagonal
-    if(sx == ex or sy == ey):
-        for x in range(N):
-            for y in range(N):
-                if x >= sx and x <= ex:
-                    if y >= sy and y <= ey:
+    if(ssx == eex or ssy == eey):
+        for x in range(ssx,eex+1):
+            for y in range(ssy,eey+1):
+                if x >= ssx and x <= eex:
+                    if y >= ssy and y <= eey:
                         map[y][x] = map[y][x] + 1
-    
-# printMap()
+    # diagonal
+    if((sx != ex) and (sy != ey)):
+        for x in range(min(sx,ex),max(sx,ex)+1):
+            if (sx < ex and sy < ey) or (sx > ex and sy > ey):
+                y = min(sy,ey) + x - min(sx,ex)
+            else:
+                y = max(sy,ey) - (x - min(sx,ex))
+            map[y][x] = map[y][x] + 1
+                    
+if N == 10:
+     printMap()
 dangerous = 0
 for x in range(N):
     for y in range(N):
